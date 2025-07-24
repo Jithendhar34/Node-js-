73d95390ddae09ff1ff7d349399ae6f133f6bf54
KJithendhar <jithendharkampati04@gmail.com>
2025-07-24 11:25:15 +0530

    second

--- index.js
+++ index.js
@@ -1,35 +1,44 @@
-/*const http = require('http');
+/*
 
+
+const http = require('http');
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello, World!\n');
 });
-
 server.listen(3000, () => {
   console.log('Server running on http://localhost:3000/');
 });
 
 
+
 */
 
-// file system 
 
 
 
-/*const fs=require('fs');
+
+/*
+const fs=require('fs');
 fs.readFile('hi.txt',(err,data)=>{
     if(err) throw err;
     console.log(data.toString());
-
 })
 */
 
-const fs = require('fs');
 
+
+/*
+
+const fs = require('fs');
 fs.writeFile('example.txt', 'Hello, World!', (err) => {
   if (err) throw err;
   console.log('Data written to file');
 });
+
+*/
+
+console.log("maintaing streak")
 
 
